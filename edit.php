b8242a2c1147df901a1c284aa70607700c0b9458
Josemaria Bolanos <josemabol@gmail.com>
2025-03-25 15:28:56 -0600

    Fix deprecation error in Moodle 4.5

--- edit.php
+++ edit.php
@@ -76,12 +76,6 @@ if ($usernew = $userform->get_data()) {
                 throw new \moodle_exception('cannotupdatepasswordonextauth', '', '', $usernew->auth);
             }
             unset_user_preference('create_password', $usernew); // Prevent cron from generating the password.
-
-            if (!empty($CFG->passwordchangelogout)) {
-                // We can use SID of other user safely here because they are unique,
-                // the problem here is we do not want to logout admin here when changing own password.
-                \core\session\manager::kill_user_sessions($usernew->id, session_id());
-            }
         }
     }
 
